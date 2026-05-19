641e4c0c1362b3d733faf5ed5972b97a185161b0
PratikC54 <pratiksenapati54@gmail.com>
2026-05-19 22:14:26 +0530

    DTO for register an user

---
+++ NanoURL/src/main/java/com/PratikC54/NanoURL/dto/RegisterRequest.java
@@ -0,0 +1,13 @@
+package com.PratikC54.NanoURL.dto;
+
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private Set<String> role;
+    private String password;
+}
