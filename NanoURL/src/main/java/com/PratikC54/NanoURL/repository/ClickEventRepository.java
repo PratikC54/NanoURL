3e6b79a0aabc4003d3ad90f27a340f6bc991f970
PratikC54 <pratiksenapati54@gmail.com>
2026-05-21 10:29:04 +0530

    creating repository for a click event model

--- NanoURL/src/main/java/com/PratikC54/NanoURL/repository/ClickEventRepository.java
+++ NanoURL/src/main/java/com/PratikC54/NanoURL/repository/ClickEventRepository.java
@@ -1,4 +1,15 @@
 package com.PratikC54.NanoURL.repository;
 
-public interface ClickEventRepository {
+import com.PratikC54.NanoURL.models.ClickEvent;
+import com.PratikC54.NanoURL.models.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface ClickEventRepository extends JpaRepository<ClickEvent, Long> {
+    List<ClickEvent> findByUrlMappingAndClickDateBetween(UrlMapping mapping, LocalDateTime startDate, LocalDateTime endDate);
+    List<ClickEvent> findByUrlMappingInAndClickDateBetween(List<UrlMapping> mapping, LocalDateTime startDate, LocalDateTime endDate);
 }
