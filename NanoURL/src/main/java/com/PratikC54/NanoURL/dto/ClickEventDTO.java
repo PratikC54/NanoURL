de6ddf3e0ba5585010cd15a2582bccb4a9dd0978
PratikC54 <pratiksenapati54@gmail.com>
2026-05-21 10:28:52 +0530

    creating DTO for a click event model

--- NanoURL/src/main/java/com/PratikC54/NanoURL/dto/ClickEventDTO.java
+++ NanoURL/src/main/java/com/PratikC54/NanoURL/dto/ClickEventDTO.java
@@ -1,4 +1,13 @@
 package com.PratikC54.NanoURL.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class ClickEventDTO {
+
+    private LocalDate clickDate;
+    private Long count;
+
 }
