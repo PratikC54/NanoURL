d2eb2ab85f330484f0751a6953e73ffb5fbd6d8d
PratikC54 <pratiksenapati54@gmail.com>
2026-05-21 10:31:29 +0530

    creating redirection logic using UrlMappingService

--- NanoURL/src/main/java/com/PratikC54/NanoURL/controller/RedirectController.java
+++ NanoURL/src/main/java/com/PratikC54/NanoURL/controller/RedirectController.java
@@ -1,4 +1,27 @@
 package com.PratikC54.NanoURL.controller;
 
+import com.PratikC54.NanoURL.models.UrlMapping;
+import com.PratikC54.NanoURL.service.UrlMappingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class RedirectController {
+    private final UrlMappingService urlMappingService;
+
+    @GetMapping("/{shortUrl}")
+    private ResponseEntity<Void> redirect(@PathVariable String shortUrl) {
+        UrlMapping urlMapping = urlMappingService.getOriginalUrl(shortUrl);
+        if (urlMapping!=null){
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Location", urlMapping.getOriginalUrl());
+            return ResponseEntity.status(302).headers(headers).build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
