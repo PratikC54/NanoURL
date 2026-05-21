23661d7444afce15e5672ff48841061f9eb3aec8
PratikC54 <pratiksenapati54@gmail.com>
2026-05-21 10:32:08 +0530

    creating redirection logic using UrlMappingService and HttpHeaders

--- NanoURL/src/main/java/com/PratikC54/NanoURL/controller/RedirectController.java
+++ NanoURL/src/main/java/com/PratikC54/NanoURL/controller/RedirectController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class RedirectController {
+
     private final UrlMappingService urlMappingService;
 
     @GetMapping("/{shortUrl}")
