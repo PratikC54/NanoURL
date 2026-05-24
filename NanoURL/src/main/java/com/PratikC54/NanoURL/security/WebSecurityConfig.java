17f5f77b55151ddf0c0f9c6431deb01bd9712994
PratikC54 <pratiksenapati54@gmail.com>
2026-05-24 22:48:24 +0530

    updating security filters

--- NanoURL/src/main/java/com/PratikC54/NanoURL/security/WebSecurityConfig.java
+++ NanoURL/src/main/java/com/PratikC54/NanoURL/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import com.PratikC54.NanoURL.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -39,6 +40,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/urls/**").permitAll()
                         .requestMatchers("/{shortUrl}").permitAll()
