2e80f9bf46bf676fa569824841b6436276aa5b6e
PratikC54 <pratiksenapati54@gmail.com>
2026-05-24 22:47:51 +0530

    updating CORS configuration

--- NanoURL/src/main/java/com/PratikC54/NanoURL/security/WebConfig.java
+++ NanoURL/src/main/java/com/PratikC54/NanoURL/security/WebConfig.java
@@ -1,7 +1,6 @@
 package com.PratikC54.NanoURL.security;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
