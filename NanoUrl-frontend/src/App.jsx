84bdaf9469208d0a3598d928761b786a0d337867
PratikC54 <pratiksenapati54@gmail.com>
2026-05-26 21:59:55 +0530

    updating helper

--- NanoUrl-frontend/src/App.jsx
+++ NanoUrl-frontend/src/App.jsx
@@ -1,53 +1,13 @@
-import { useEffect } from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
-import LandingPage from "./components/LandingPage.jsx";
-import AboutPage from "./components/AboutPage.jsx";
-import LoginPage from "./components/LoginPage.jsx";
-import RegisterPage from "./components/RegisterPage.jsx";
-import DashboardLayout from "./Dashboard/DashboardLayout.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { getApps } from "./utils/helper.js";
+import "./App.css";
 
-import { Toaster } from "react-hot-toast";
-
-function ScrollToHash() {
-  const { pathname, hash } = useLocation();
-
-  useEffect(() => {
-    if (hash) {
-      const id = hash.replace("#", "");
-      const timer = setTimeout(() => {
-        document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
-      }, 100);
-      return () => clearTimeout(timer);
-    }
-    window.scrollTo(0, 0);
-  }, [pathname, hash]);
-
-  return null;
-}
 
 function App() {
+  const CurrentApp = getApps();
   return (
     <BrowserRouter>
-      <Navbar />
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-        toastOptions={{
-          duration: 4000,
-          style: {
-            zIndex: 9999,
-          },
-        }}
-      />
-      <ScrollToHash />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<DashboardLayout />} />
-      </Routes>
+      <CurrentApp />
     </BrowserRouter>
   );
 }
