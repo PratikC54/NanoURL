fa6b9e165752bf48c0d16487d590e051b4317842
PratikC54 <pratiksenapati54@gmail.com>
2026-05-26 22:06:35 +0530

    created routing for sub-domain

---
+++ NanoUrl-frontend/src/utils/helper.js
@@ -0,0 +1,18 @@
+import { subDomainList } from "./Constant.js";
+
+export const getApps = () => {
+    const subDomain = getSubDomain(window.location.hostname);
+    const mainApp = subDomainList.find((app) => app.main);
+    if (subDomain === "") {
+        return mainApp.app;
+    }
+    const apps = subDomainList.find((app) => app.subdomain === subDomain);
+    return apps ? apps.app : mainApp.app;
+}
+
+export const getSubDomain = (location) => {
+    const locationParts = location.split(".");
+    const isLocalhost = locationParts[0] === "localhost";
+    const sliceTill = isLocalhost ? -1 : -2;
+    return locationParts.slice(0, sliceTill).join("");
+};
