02507b66479b55a0f1e287e8521513fa33374bd9
PratikC54 <pratiksenapati54@gmail.com>
2026-05-27 20:59:26 +0530

    Creating error page

---
+++ NanoUrl-frontend/src/components/ErrorPage.jsx
@@ -0,0 +1,28 @@
+import { FaExclamationTriangle } from 'react-icons/fa'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+const ErrorPage = ({ message }) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const displayMessage = message || location.state?.message || "An unexpected error has occured";
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)] bg-gray-100 p-6">
+        <FaExclamationTriangle className='text-6xl text-red-500 mb-4' />
+        <h1 className='text-3xl font-bold mb-2 text-gray-800'>
+            Oops! Something went wrong.
+        </h1>
+        <h2 className='text-gray-600 mb-6 text-center'>
+            {displayMessage}
+        </h2>
+        <button onClick={() => {
+            navigate("/");
+        }}
+        className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition'
+        >
+            Go back to home
+        </button>
+    </div>
+  )
+}
+
+export default ErrorPage
