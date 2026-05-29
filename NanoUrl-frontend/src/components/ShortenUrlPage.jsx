7f64c03def3549cf19105440c6bf3b102eb61db5
PratikC54 <pratiksenapati54@gmail.com>
2026-05-29 13:56:33 +0530

    Adding redirect hourglass

--- NanoUrl-frontend/src/components/ShortenUrlPage.jsx
+++ NanoUrl-frontend/src/components/ShortenUrlPage.jsx
@@ -41,6 +41,8 @@ const ShortenUrlPage = () => {
           colors={["#306cce", "#72a1ed"]}
         />
         <h1 className="text-2xl font-bold mt-4">Redirecting...</h1>
+        
+        <h1 >Redirecting...</h1>
       </div>
     </div>
   );
