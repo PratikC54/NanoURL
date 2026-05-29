6d17e1f89ebc755caf5da5bdb2871b7d19ac08aa
PratikC54 <pratiksenapati54@gmail.com>
2026-05-29 13:50:14 +0530

    Updating length of original url

--- NanoURL/src/main/java/com/PratikC54/NanoURL/dto/UrlMappingDTO.java
+++ NanoURL/src/main/java/com/PratikC54/NanoURL/dto/UrlMappingDTO.java
@@ -1,12 +1,15 @@
 package com.PratikC54.NanoURL.dto;
 
 import lombok.Data;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
 @Data
 public class UrlMappingDTO {
     private Long id;
+
+    @Column(length = 500)
     private String originalurl;
     private String shorturl;
     private int clickCount;
